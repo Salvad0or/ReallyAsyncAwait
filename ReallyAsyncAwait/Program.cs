224b85d219ea3043ced45e684847a2cecc0cab40
- <wavevape@gmail.com>
2022-10-11 23:01:30 +0300

    11.10

--- ReallyAsyncAwait/Program.cs
+++ ReallyAsyncAwait/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ReallyAsyncAwait
 {
@@ -6,7 +8,46 @@ namespace ReallyAsyncAwait
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0;
+
+            int b = a = 1;
+
+            Console.WriteLine($"A - {a}, B - {b}");
+
+            Console.ReadKey();
+
+            Console.WriteLine($"Метод Main начал свою работу в потоке {Thread.CurrentThread.ManagedThreadId}");
+
+            WriteCharAsync('#'); // Запуск асинхронно
+            WriteChar('*'); // Запуск синхронно
+
+            Console.WriteLine($"Метод Main закончил свою работу в потоке {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.ReadKey();
+        }
+
+        private static async Task WriteCharAsync(char symbol)
+        {
+            Console.WriteLine($"Метод WriteCharAsync начал свою работу в потоке {Thread.CurrentThread.ManagedThreadId}");
+
+            await Task.Run(() => WriteChar(symbol));
+
+            Console.WriteLine($"-------Метод WriteCharAsync закончил свою работу в потоке {Thread.CurrentThread.ManagedThreadId}");
+
+        }
+
+        private static void WriteChar(char symbol)
+        {
+            Console.WriteLine($"Метод WriteChar Id потока - {Thread.CurrentThread.ManagedThreadId} , Id задачи - {Task.CurrentId}");
+
+            Thread.Sleep(500);
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(symbol);
+                Thread.Sleep(100);
+            }
+
         }
     }
 }
