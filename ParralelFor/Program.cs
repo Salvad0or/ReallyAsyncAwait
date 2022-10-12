64a96a62e8c7bbac65cbbe55667ab7a9e83ad6ec
- <wavevape@gmail.com>
2022-10-12 18:55:42 +0300

    12.10 We continue this very important course

---
+++ ParralelFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParralelFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(1, 5, TplTest1);
+
+            Console.ReadKey();
+
+
+        }
+
+        static void TplTest1(int i)
+        {
+            Console.WriteLine($"Метод ParallelFor передал {i}");
+        }
+
+        /// Метод будет выполнятся параллельно
+        /// 1 - это начальная точка итераций
+        /// 5 - до какого числа не включительно будет продолжаться итерация
+        /// ну и сам метод принимающий значения от 1 - 4.
+    }
+}
---
+++ TroelsenTimer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace TroelsenTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(Time,null,0,1000);
+
+            timer = new Timer(State, "Ha-ha", 0, 1000);
+
+            var _ = new Timer(State, "Ho-ho", 0, 1000); /// это вот так вот можно
+
+
+            Console.ReadLine();
+        }
+
+
+        public static void Time(object _)
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        }
+
+        public static void State(object state)
+        {
+            /// здесь мы вызываем таймер, и вместо null что то ему передаем
+            /// учитывая, что это obj - можно творить магию
+            /// но я слишком ленив и кинул стринг хаха
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write(state);
+
+            }
+
+        }
+    }
+}
