b5084b2d30ff73fe94c712f316d306753241217e
- <wavevape@gmail.com>
2022-10-14 22:06:31 +0300

    PLINQ

--- PLINQ_001/Program.cs
+++ PLINQ_001/Program.cs
@@ -8,11 +8,11 @@ namespace PLINQ_001
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             int[] mass = Enumerable.Range(1,1000000).ToArray();
 
-            var result = (from m in mass
+            var result = (from m in mass.AsParallel() // Здесь происходит распараллеливание, если это возможно.
                          where m % 3 == 0
                          orderby m descending
                          select m).ToArray();
