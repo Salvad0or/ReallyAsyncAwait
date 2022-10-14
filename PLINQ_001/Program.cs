1573e0649b2521ddcfa55a6ff675cd9c40c4587e
- <wavevape@gmail.com>
2022-10-14 22:04:51 +0300

    14.10 PLINQ

---
+++ PLINQ_001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLINQ_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = Enumerable.Range(1,1000000).ToArray();
+
+            var result = (from m in mass
+                         where m % 3 == 0
+                         orderby m descending
+                         select m).ToArray();
+
+            Console.WriteLine(result.Count());
+
+            Console.ReadKey();
+
+        }
+    }
+}
